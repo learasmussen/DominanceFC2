4b40b34b70b3e97e54234ce0e09f688d4864c764
lea03 <ler002@edu.zealand.dk>
2025-03-11 13:44:43 +0100

    smidt footer i en include

--- index.php
+++ index.php
@@ -120,6 +120,7 @@ require "settings/init.php";
     </div>
 </div>
 
+<!-- Mark O Madsen CEO plus " section-->
 <div class="container px-2 px-md-3 mb-3 d-none d-md-block">
     <div class="row">
         <div class="col-3 pe-2 ps-3 ps-md-2 mt-lg-2 pe-lg-3">
@@ -190,65 +191,7 @@ require "settings/init.php";
     </div>
 </div>
 
-<!-- Footer-->
-<footer class="container-fluid bg-black text-white py-4">
-    <div class="container text-center">
-        <!-- Newsletter Signup -->
-        <h5 class="text-uppercase fw-bold" id="sign-up-for-newsletter-header">Sign up for Dominance FC Newsletter</h5>
-
-        <div class="row justify-content-center align-items-center mt-3 px-2 py-0">
-
-                <label class="col-md-7 col-8 p-0">
-                    <input type="email" class="form-control bg-white text-light-gray rounded-0 email-subscribe " placeholder="E-MAIL" required>
-                </label>
-
-            <div class="col-md-5 col-4 p-0">
-                <button type="submit" class="btn btn-orange text-uppercase fw-bold w-100 rounded-0  email-subscribe text-white">Subscribe</button>
-            </div>
-        </div>
-
-
-        <!-- Orange Line -->
-        <div class="border-top border-orange mt-4" style="border-width: 3px;"></div>
-
-        <!-- Footer Links -->
-        <div class="row text-start mt-4">
-            <!-- Event Related -->
-            <div class="col-3">
-                <div class="text-uppercase fw-bold footer-headers" >Event Related</div>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Tickets</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Events</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Fighter Sign Up</a></p>
-            </div>
-
-            <!-- Information -->
-            <div class="col-3">
-                <div class="text-uppercase fw-bold footer-headers">Information</div>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Sponsors</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Partners</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">About</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Cookie Policy (EU)</a></p>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-none footer-text">Privacy Statement (EU)</a></p>
-            </div>
-
-            <!-- Contact -->
-            <div class="col-3">
-                <div class="text-uppercase fw-bold footer-headers">Get in Contact</div>
-                <p class="mb-1"><a href="#" class="text-white text-decoration-underline fw-bold footer-text">Contact</a></p>
-                <p class="text-white footer-text">CVR: <a href="tel:44744658" class="text-white text-decoration-none footer-text">44744658</a></p>
-            </div>
-
-            <!-- Socials -->
-            <div class="col-3">
-                <div class="text-uppercase fw-bold footer-headers">Socials</div>
-                <p class="mb-1"><a href="https://instagram.com" class="text-white text-decoration-none footer-text"><i class="fa-brands fa-instagram text-orange"></i>  Instagram</a></p>
-                <p class="mb-1"><a href="https://facebook.com" class="text-white text-decoration-none footer-text"><i class="fa-brands fa-facebook text-orange"></i>  Facebook</a></p>
-                <p class="mb-1"><a href="https://tiktok.com" class="text-white text-decoration-none footer-text"><i class="fa-brands fa-tiktok text-orange"></i>  TikTok</a></p>
-            </div>
-        </div>
-    </div>
-</footer>
-
+<?php include("include/footer.php") ?>
 
 <script src="navbar.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
