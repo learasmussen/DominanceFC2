08f5b03d005fb160d3a329a151c7d0596268ca8c
lea03 <ler002@edu.zealand.dk>
2025-03-12 18:34:32 +0100

    lavet about side

---
+++ about.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>AIRTOX DOMINANCE FC</title>
+
+        <meta name="robots" content="All">
+        <meta name="author" content="Udgiver">
+        <meta name="copyright" content="Information om copyright">
+
+        <link href="css/styles.css" rel="stylesheet" type="text/css">
+        <script src="https://kit.fontawesome.com/4e7ccd0dde.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+
+<?php include("include/logo-navbar.php") ?>
+
+<!-- Top image-->
+<div class="container-fluid position-relative">
+    <div class="row">
+        <div class="col-12 p-0 position-relative">
+            <!-- Text positioned on top-left -->
+            <div class="top-image-text position-absolute font-overtext top-0 start-0 text-orange fw-bold p-3 p-md-4 p-lg-5">
+                ABOUT
+            </div>
+            <!-- Background Image -->
+            <img src="images/about-top-image.jpeg" alt="about-top-image" class="w-100">
+        </div>
+    </div>
+</div>
+
+<!-- What is Dominance fc-->
+<div class="container px-1 mt-3 mt-md-5">
+    <div class="row mx-2">
+        <div class="col-7 ps-0 pe-2">
+            <div class="fw-bold font-overtext" id="what-is-dominance-fc-header">WHAT IS DOMINANCE FC?</div>
+            <p class="what-is-dominance-fc-text font-text fw-bold">DFC, short for Dominance Fighting Championship, was established by three-time Olympian and UFC veteran Mark O. Madsen. Dominance FC’s vision is to establish Europe’s premier fight promotion, hosting multiple Dominance FC events annually. Dominance FC is dedicated to advancing the sport of MMA in Europe, creating exceptional experiences for fighters and fans alike.</p>
+        </div>
+
+        <div class="col-5 ps-0 pe-0">
+            <img src="images/what-is-dominance-fc-image.jpg" alt="what-is-dominance-fc-image" class="col-12">
+        </div>
+    </div>
+</div>
+
+<!-- Mark O Madsen CEO plus " section-->
+<div class="container px-2 px-md-3 mb-3 mb-md-5">
+    <div class="row">
+        <div class="col-3 pe-2 ps-3 ps-md-2 mt-lg-2 pe-lg-3">
+            <img src="images/mark-o-madsen.jpg" alt="mark-o-madsen" class="col-12 rounded-5 ">
+        </div>
+        <div class="col-3 p-0 d-flex flex-column justify-content-center align-items-start">
+            <p class="text-orange fw-bold mark-o-madsen-ceo mb-0 font-overtext">MARK O MADSEN</p>
+            <p class="text-orange fw-bold mark-o-madsen-ceo mb-0 font-overtext">CEO OF DOMINANCE FC</p>
+            <img src="images/mark-o-madsen-signature.png" alt="mark-o-madsen-signature" class="col-12 rounded-5">
+        </div>
+        <div class="col-1 text-light-gray p-0 text-center font-overtext" id="apostrophie">"</div>
+        <div class="col-5 p-0 pe-3 ">
+            <p class="what-is-dominance-fc-text mb-0 pt-1 font-text fw-bold">Dominance FC strives to build a fight promotion that attracts the best European fighters and delivers outstanding experiences for both spectators and sponsors. Our goal is to organize adrenaline-pumping MMA shows where fight fans experience events beyond the ordinary.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Why Dominance fc-->
+<div class="container px-1 my-3 my-md-5">
+    <div class="row mx-2">
+        <div class="col-8 ps-0 pe-2">
+            <div class="fw-bold font-overtext text-orange mb-3" id="what-is-dominance-fc-header">WHY DOMINANCE FC?</div>
+            <p class="what-is-dominance-fc-text font-text fw-bold pe-3 mb-2">MMA, or Mixed Martial Arts, is the world’s fastest-growing sport, and we see enormous potential in the European MMA scene. </p>
+            <p class="what-is-dominance-fc-text font-text fw-bold pe-3 mb-0">Dominance FC aims to create a platform that matches this growth, promotes the sport in Europe, and provides European talents with an international stage to develop their potential and build their brand.</p>
+        </div>
+
+        <div class="col-4 ps-0 pe-0">
+            <img src="images/why-dominance-fc-image.jpg" alt="why-dominance-fc-image" class="col-12">
+        </div>
+    </div>
+</div>
+
+<!-- What do we aim to achieve-->
+<div class="container-fluid px-1 my-3 my-md-5 mx-0 bg-dark-gray">
+    <div class="row mx-2 py-3 py-lg-4">
+
+        <div class="col-6 ps-0 pe-0 ps-md-3">
+            <img src="images/what-do-we-aim-to-achieve-image.jpeg" alt="what-do-we-aim-to-achieve-image" class="col-12">
+        </div>
+
+        <div class="col-6 ps-3 pe-0">
+            <div class="fw-bold font-overtext text-orange mb-md-3 mb-lg-4" id="what-do-we-aim-to-achieve-header">WHAT DO WE AIM TO ACHIEVE?</div>
+            <p class=" font-text pe-3 mb-1 mb-md-3 what-do-we-aim-to-achieve-text text-white">Dominance FC strives to build a fight promotion that attracts the best European fighters and delivers outstanding experiences for both spectators and sponsors.</p>
+            <p class=" font-text pe-3 mb-1 mb-md-3 what-do-we-aim-to-achieve-text text-white">Our goal is to organize adrenaline-pumping MMA shows where fight fans experience events beyond the ordinary. Dominance FC aspires to be the preferred choice for both fighters and fans on the European scene, creating an atmosphere filled with intensity and excitement.</p>
+            <p class=" font-text pe-3 mb-1 mb-md-3 what-do-we-aim-to-achieve-text text-white">Initially, Dominance FC will focus on MMA shows in Denmark, available via streaming. Dominance FC dreams of being known for the most exciting and unique MMA shows for fighters, audiences, sponsors, and partners.</p>
+            <p class=" font-text pe-3 mb-1 mb-md-3 what-do-we-aim-to-achieve-text text-white">We aim to build a strong presence on social media, where fighters can market themselves, and spectators can follow the fascinating narratives that make the fights even more interesting. </p>
+            <p class=" font-text pe-3 mb-0 what-do-we-aim-to-achieve-text text-white">Dominance FC strives to become an internationally recognized platform where talent and passion meet in spectacular battles.</p>
+        </div>
+    </div>
+</div>
+
+<?php include("include/footer.php") ?>
+
+<script src="navbar.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
